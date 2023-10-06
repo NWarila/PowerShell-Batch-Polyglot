80a940871826fe9d145f235d789516ffe6d83f07
Smarter  Harder <33955773+NWarila@users.noreply.github.com>
2023-10-06 11:37:47 -0400

    Update Polyglot.bat
    Added ?getHelp parameter to add PowerShell help text support.
    Migrated comments to '@REM' instead of :#
    The '?noCLS' parameter now does something.

--- Polyglot.bat
+++ Polyglot.bat
@@ -1,65 +1,34 @@
-<# ::
-@ECHO OFF & Rem/||(
+<# :: Start of the PS comment around the Batch Section ###########################################################
+@ECHO OFF
 
-    .SYNOPSIS
-    Is used to install an HTTPS WSMan Listener on a computer with a valid certificate.
-
-    .DESCRIPTION
-        This script is designed to be called from a Startup/Logon PowerShell GPO.
-        The Distinguished Name of the certificate issuer must be passed to the script.
-
-    .PARAMETER Issuer
-    The full Distinguished Name of the Issing CA that will have issued the certificate to be used
-    for this HTTPS WSMan Listener.
-
-    .PARAMETER DNSNameType
-    The allowed DNS Name types that will be used to find a matching certificate. Defaults to Both.
-
-    .PARAMETER MatchAlternate
-    The certificate used must also have an alternate subject name containing the DNS name found in
-    the subject as well. Defaults to false.
-
-    .PARAMETER Port
-    This is the port the HTTPS WSMan Listener will be installed onto. Defaults to 5986.
-
-    .PARAMETER LogFilename
-    This optional parameter contains a full path and file name to the log file to create.
-    If this parameter is not set then a log file will not be created.
-
-    .EXAMPLE
-    Install-WSManHttpsListener -Issuer 'CN=CONTOSO.COM Issuing CA, DC=CONTOSO, DC=COM'
-    Install a WSMan HTTPS listener from an appropriate machine certificate issued by
-    'CN=LABBUILDER.COM Issuing CA, DC=LABBUILDER, DC=COM'.
-
-    .EXAMPLE
-    Install-WSManHttpsListener -Issuer 'CN=CONTOSO.COM Issuing CA, DC=CONTOSO, DC=COM' -Port 7000
-    Install a WSMan HTTPS listener from an appropriate machine certificate issued by
-    'CN=LABBUILDER.COM Issuing CA, DC=LABBUILDER, DC=COM' on port 7000.
-
-
-)
-
-:# Disabling argument expansion avoids issues with ! in arguments.
+@REM Disabling argument expansion avoids issues with ! in arguments.
 SetLocal EnableExtensions DisableDelayedExpansion
 
-:# Temporarily Ignore Zone Checking
+@REM Temporarily Ignore Zone Checking
 SET "SEE_MASK_NOZONECHECKS=1"
 
-:# Define Intial Variable(s)
-FOR %%V in (noCLS noWait isUNC didTimeout) DO (SET "%%V=FALSE")
-SET "startDir=%cd%" & SET "scriptDir=%~dp0"
+@REM Define Intial Variable(s)
+FOR %%V in (noCLS noWait isUNC didTimeout NoAdmin getHelp) DO (SET "%%V=FALSE")
+SET "ARGS=%*" & SET "startDir=%cd%" & SET "scriptDir=%~dp0" & SET "FilePath=%~f0"
 
-:# Extract Non-PowerShell Variables.
-@ECHO. %ARGS% | Findstr /I /C:"\?NoCLS">nul && (SET "NoCLS=TRUE" & SET "ARGS=%ARGS:?NoCLS =%")
-@ECHO. %ARGS% | Findstr /I /C:"\?NoWait">nul && (SET "NoWait=TRUE" & SET "ARGS=%ARGS:?NoWait =%")
-@ECHO. %ARGS% | Findstr /I /C:"\?NoAdmin">nul && (SET "NoAdmin=TRUE" & SET "ARGS=%ARGS:?NoAdmin =%")
+@REM Extract Non-PowerShell Variables.
+ECHO. %ARGS% | Findstr /I /C:" \?NoCLS">nul && (SET "NoCLS=TRUE" & SET "ARGS=%ARGS:?NoCLS=%")
+ECHO. %ARGS% | Findstr /I /C:" \?NoWait">nul && (SET "NoWait=TRUE" & SET "ARGS=%ARGS:?NoWait=%")
+ECHO. %ARGS% | Findstr /I /C:" \?NoAdmin">nul && (SET "NoAdmin=TRUE" & SET "ARGS=%ARGS:?NoAdmin=%")
+ECHO. %ARGS% | Findstr /I /C:" \?GetHelp">nul && (SET "getHelp=TRUE" & SET "ARGS=%ARGS:?GetHelp=%")
 
-:# Prepare the batch arguments, so that PowerShell parses them correctly
-SET ARGS=%*
-IF defined ARGS set ARGS=%ARGS:"=\"%
-IF defined ARGS set ARGS=%ARGS:'=''%
+@REM Clear the screen up.
+If /I "%NoCLS%" == "False" ( CLS )
 
-:# Enforce Elevation If Required.
+@REM Prepare the batch arguments, so that PowerShell parses them correctly
+If /I "%getHelp%" == "True" (
+    SET "ARGS=-ShowHelp"
+) Else (
+    IF defined ARGS set ARGS=%ARGS:"=\"%
+    IF defined ARGS set ARGS=%ARGS:'=''%
+)
+
+@REM Enforce Elevation If Required.
 IF "%NoAdmin%" == "FALSE" (
     net session >nul 2>&1 || (
         ECHO. Script requires elevation
@@ -68,43 +37,35 @@ IF "%NoAdmin%" == "FALSE" (
     )
 )
 
-:# Escape the file path for all possible invalid characters.
-SET "FilePath=%FilePath:'=''%"
-SET "FilePath=%FilePath:^=^^%"
-SET "FilePath=%FilePath:[=`[%"
-SET "FilePath=%FilePath:]=`]%"
-SET "FilePath=%FilePath:&=^&%"
-
-:# Ensure path is utilizing a lettered drive path.
-SET "FilePath=%~f0"
-IF "%FilePath:~0,2%" == "\\" PUSHD "%~dp0"
-IF "%FilePath:~0,2%" == "\\" SET "FilePath=%CD%\%~nx0"
-IF NOT "%FilePath:~0,2%" == "\\" CD "%~dp0"
-
-:# Always move to local directory.
-IF "%StartDir:%~0,2%" == "\\" (
-    SET "isUNC=True"
+@REM Ensure path is utilizing a lettered drive path.
+IF "%FilePath:~0,2%" == "\\" DO (
     PUSHD "%~dp0"
-) ELSE (
+    SET "isUNC=True"
+) Else (
     If NOT "%scriptDir%" == "%startDir" (
         CD /D "%~dp0"
     )
 )
+SET "FilePath=%CD%\%~nx0"
 
-:# ============================================================================================================ #:
-:# The ^ before the first " ensures that the Batch parser does not enter quoted mode there, but that it enters  #:
-:# and exits quoted mode for every subsequent pair of ". This in turn protects the possible special chars & | < #:
-:# > within quoted arguments. Then the \ before each pair of " ensures that PowerShell's C command line parser  #:
-:# considers these pairs as part of the first and only argument following -c. Cherry on the cake, it's possible #:
-:# to pass a " to PS by entering two "" in the bat args.                                                        #:
-:# ============================================================================================================ #:
-ECHO In BATCH; Entering PowerShell.
+@REM Escape the file path for all possible invalid characters.
+SET "FilePath=%FilePath:'=''%" & SET "FilePath=%FilePath:^=^^%" & SET "FilePath=%FilePath:[=`[%"
+SET "FilePath=%FilePath:]=`]%" & SET "FilePath=%FilePath:&=^&%"
+
+@REM ========================================================================================================== #:
+@REM The ^ before the first " ensures that the Batch parser does not enter quoted mode there, but that it       #:
+@REM enters and exits quoted mode for every subsequent pair of ". This in turn protects the possible special    #:
+@REM chars & | < within quoted arguments. Then the \ before each pair of " ensures that PowerShell's C command  #:
+@REM line parser considers these pairs as part of the first and only argument following -c. Cherry on the cake, #:
+@REM it's possible to pass a " to PS by entering two "" in the bat args.                                        #:
+@REM ========================================================================================================== #:
+::@ECHO In BATCH; Entering PowerShell.
 "%WinDir%\System32\WindowsPowerShell\v1.0\powershell.exe" -c ^
     ^"Invoke-Expression ('^& {' + (get-content -raw '%FilePath%') + '} %ARGS%')"
-ECHO Exited PowerShell; Back in BATCH.
+::@ECHO Exited PowerShell; Back in BATCH.
 
-:# Wait 60 seconds before closing.
-IF "%noWait%" == "FALSE" (
+@REM Wait 60 seconds before closing.
+IF NOT "%noWait%" == "TRUE" (
     TIMEOUT /T 60
 )
 
@@ -119,10 +80,49 @@ if "%isUNC%" == "TRUE" (
     )
 )
 
-@endlocal
-GOTO:END
+@REM Exit Script
+@endlocal & CALL ::EOF & EXIT /B
 
-:# ###############################################################################################################
-:# #>
+:# ############### End of the PS comment around the Batch section; Begin the PowerShell section. ############ : #>
+Param (
+    [Switch]$ShowHelp
+)
 
-:END
+If ($ShowHelp -eq $True) {
+    Write-Host -Object:@'
+    NAME
+        Get-HotFix
+
+    SYNOPSIS
+        Gets the hotfixes that have been applied to the local and remote computers.
+
+
+    SYNTAX
+        Get-HotFix [-ComputerName <String[]>] [-Credential <PSCredential>] [-Description
+        <String[]>] [<CommonParameters>]
+
+        Get-HotFix [[-Id] <String[]>] [-ComputerName <String[]>] [-Credential
+        <PSCredential>] [<CommonParameters>]
+
+
+    DESCRIPTION
+        The Get-Hotfix cmdlet gets hotfixes (also called updates) that have been installed
+        on either the local computer (or on specified remote computers) by Windows Update,
+        Microsoft Update, or Windows Server Update Services; the cmdlet also gets hotfixes
+        or updates that have been installed manually by users.
+
+
+    RELATED LINKS
+        Online Version: http://go.microsoft.com/fwlink/?LinkId=821586
+        Win32_QuickFixEngineering http://go.microsoft.com/fwlink/?LinkID=145071
+        Get-ComputerRestorePoint
+        Add-Content
+
+    REMARKS
+        To see the examples, type: "get-help Get-HotFix -examples".
+        For more information, type: "get-help Get-HotFix -detailed".
+        For technical information, type: "get-help Get-HotFix -full".
+        For online help, type: "get-help Get-HotFix -online"
+'@
+Exit 0
+}
